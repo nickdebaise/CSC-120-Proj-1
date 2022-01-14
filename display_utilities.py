7f296f2de7617e16a74f381584d432d421233de1
Nick DeBaise <nickdebaise@gmail.com>
2022-01-13 21:13:19 -0500

    Fix commas issue

--- display_utilities.py
+++ display_utilities.py
@@ -7,6 +7,7 @@ def get_display_data(num_pairs, num_2_pairs, num_flushes, num_high_cards, number
     return [number_items, num_pairs, num_pairs / number_items * 100, num_2_pairs, num_2_pairs / number_items * 100,
             num_flushes, num_flushes / number_items * 100, num_high_cards, num_high_cards / number_items * 100]
 
+
 def print_headers():
     print("{:>10}  {:>8}  {:^5}  {:>10}  {:^5}  {:>10}  {:^5}  {:>10}  {:^5}".format(
         "# of hands", "pairs", "%", "2 pairs", "%", "flushes", "%", "high card", "%"
@@ -23,6 +24,7 @@ def print_row(data):
     print("{:>10,d}  {:>8}  {:0>5.2f}  {:>10}  {:0>5.2f}  {:>10}  {:0>5.2f}  {:>10}  {:0>5.2f}".format(
         data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]
     ))
+
 
 if __name__ == "__main__":
     print_headers()
